2be0ffa17ed40c52256ee413aef761670aa26581
prudhvirajvarri <varri.prudhviraj.21033@iitgoa.ac.in>
2025-08-03 15:41:42 +0530

    Corrections.

--- memory_manager.py
+++ memory_manager.py
@@ -1,21 +1,31 @@
 import chromadb
 import uuid
 import time
+import os
 
 client = chromadb.PersistentClient(path = "memory_db")
 
 memory_collection = client.get_or_create_collection(
     name = "memory_collection",
-    metadata = {
-        "hnsw": {
-            "space" : "cosine"
-        }
-    }
+    metadata = {"hnsw:space" : "cosine"}
 )
-# max_memory = 10
+max_memory = 1
+
+def get_file_size(file_path = "memory_db"):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(file_path):
+        for filename in filenames:
+            file_size = os.path.getsize(os.path.join(dirpath, filename))
+            total_size += file_size
+    # Convert size from bytesto GB
+    total_size_gb = total_size / (1024 ** 3)
+    return total_size_gb
 
 def create_memory(content: str):
     try:
+        #while get_file_size() >= max_memory:
+            #print("Memory limit reached. Deleting old memories.")
+           
         curr_time = time.time()
         memory_id = str(uuid.uuid4())
         memory_collection.add(
